b87e60be8cb469a11c4c67d9b60490a9de91ffef
aditkusuma <radityakusuma2017@gmail.com>
2025-11-12 14:56:56 +0700

    Tutorial 8

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -50,7 +50,7 @@ class MyHomePage extends StatelessWidget {
                   const Padding(
                     padding: EdgeInsets.only(top: 16.0),
                     child: Text(
-                      'Selamat datang di Football News',
+                      'Welcome to Football News',
                       style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 18.0,
